83b7a5316453a673139a2bd37b736753cc117ca5
Gerold dayne Kessler <gkessler@vo-d4.21-school.ru>
2019-05-04 13:15:05 +0300

    one more

--- src/LeetCode/isAlienSorted.java
+++ src/LeetCode/isAlienSorted.java
@@ -1,4 +1,26 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class isAlienSorted {
+    public static void main(String[] args) { // решено не до конца - вернусь к нему позже
+        System.out.println(new isAlienSorted().isAlienSorted(new String[] {"word","world","row"}, "worldabcefghijkmnpqstuvxyz"));
+    }
+    public boolean isAlienSorted(String[] words, String order) {
+        if (words.length < 2) return true;
+        Map<Character, Integer> alph = new HashMap<>();
+        for (int i = 0; i < order.length(); i++){
+            alph.put(order.charAt(i), i + 1);
+        }
+        int wordSize = 0;
+        for (int i = 0; i < words.length; i++){
+            wordSize = words[i].length();
+            for (int j = 0; j + 1 < words.length; j++){
+                if (words[j + 1].length() < wordSize || (alph.get(words[j].charAt(j))) > (alph.get(words[j + 1].charAt(j))))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
